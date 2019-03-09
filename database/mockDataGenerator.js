3c05ad92cffffc0efc3ca60f0a322fbce345d217
Sean <meyersean.c@gmail.com>
2019-03-09 13:30:00 -0600

    algo not working for dataGeneration. I can still make the generation work, but it will not be as dynamic as I had hoped. Refactor time.

--- database/mockDataGenerator.js
+++ database/mockDataGenerator.js
@@ -3,9 +3,19 @@ faker.seed(31337);
 
 const createFakeItem = id => ({
   titleId: id,
-  feature: faker.company.catchPhrase(),
-  featureDescription: (() => {
-    return `${faker.hacker.phrase()} ${faker.hacker.phrase()}`;
+  features: (() => {
+    const itemFeatures = [];
+    const numOfFeatures = Math.floor(Math.random() * (6 - 2 + 1)) + 2;
+    for (i = 0; i < numOfFeatures; i++) {
+      const feature = {
+        feature: faker.company.catchPhrase(),
+        featureDescription: (() => {
+          `${faker.hacker.phrase()} ${faker.hacker.phrase()}`;
+        })()
+      };
+      itemFeatures.push(feature);
+      console.log(itemFeatures);
+    }
   })()
 });
 
@@ -17,4 +27,14 @@ const batchData = num => {
   return results;
 };
 
+console.log(batchData(4));
+
 module.exports = { createFakeItem, batchData };
+
+// const createFakeItem = id => ({
+//   titleId: id,
+//   feature1: faker.company.catchPhrase(),
+//   featureDescription1: (() => {
+//     return `${faker.hacker.phrase()} ${faker.hacker.phrase()}`;
+//   })()
+// });
